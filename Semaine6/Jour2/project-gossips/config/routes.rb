0487fe5a148cd09d7c06b4a811ef55ad20f88377
Duy DANG <tao.zen.duy@gmail.com>
2017-11-07 15:09:11 +0100

    Gossip et Devise

---
+++ Semaine6/Jour2/project-gossips/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :moussaillons
+
+  root 'home#index'
+
+  get 'home/index'
+
+  get 'home/private'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
